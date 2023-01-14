1acf8cbbeca1050311ecd0388da4636be4cc0c48
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2023-01-14 22:39:32 +0500

    Update Hamii.py

--- Hamii.py
+++ Hamii.py
@@ -4,8 +4,8 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from arch64 import main
+    from FILE_HAMII_X64 import main
     main()
 elif bit == '32bit':
-    from arch32 import main
+    from FILE_HAMII_X import main
     main()
